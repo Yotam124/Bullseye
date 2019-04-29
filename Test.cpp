72bd8a7433ec15789a35b0a7d4ce566a4902f0d0
Yotam124 <44746421+Yotam124@users.noreply.github.com>
2019-04-29 21:32:49 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -29,9 +29,22 @@ int main() {
 		ConstantChooser c1234{"1234"}, c12345{"12345"}, c9999{"9999"};
 		ConstantGuesser g1234{"1234"}, g12345{"12345"}, g9999{"9999"};
 
+		//My players
+		ConstantChooser a1234{"1234"}, d{""};
+		ConstantGuesser b1234{"1234"}, b12345{"12345"};
+
+
 		testcase.setname("Calculate bull and pgia")
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","1234"), "4,0")      // 4 bull, 0 pgia
 		.CHECK_OUTPUT(calculateBullAndPgia("1234","4321"), "0,4")      // 0 bull, 4 pgia
+
+		//My tests
+
+		.CHECK_OUTPUT(calculateBullAndPgia("2134","1111"), "1,0")      // 1 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("2134","1211"), "0,1")      // 0 bull, 1 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1231","1561"), "2,0")      // 2 bull, 0 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1231","5116"), "2,0")      // 0 bull, 2 pgia
+		.CHECK_OUTPUT(calculateBullAndPgia("1231","4444"), "0,0")      // 0 bull, 0 pgia
 		;
 
 		testcase.setname("Play with dummy choosers and guessers")
@@ -39,6 +52,10 @@ int main() {
 		.CHECK_EQUAL(play(c1234, g9999, 4, 100), 101)    // guesser loses by running out of turns
 		.CHECK_EQUAL(play(c1234, g12345, 4, 100), 101)   // guesser loses technically by making an illegal guess (too long).
 		.CHECK_EQUAL(play(c12345, g1234, 4, 100), 0)     // chooser loses technically by choosing an illegal number (too long).
+
+		//My tests
+		.CHECK_EQUAL(play(a1234, b1234, 3, 100), 1)      // A technically draw because both players chose illegal selections.
+		.CHECK_EQUAL(play(d, b1234, 0, 100), 1)			 // guesser loses technically by making an illegal guess (too long).
 		;
 
 		testcase.setname("Play with smart guesser");
