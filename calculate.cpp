574ddb6aa7a7db2203eebf88faf31eee02723c38
Yotam124 <44746421+Yotam124@users.noreply.github.com>
2019-05-07 15:34:15 +0300

    Add files via upload

--- calculate.cpp
+++ calculate.cpp
@@ -13,17 +13,28 @@ string calculateBullAndPgia(string choice, string guess){
 	int pgia = 0;
 	string newGuess = "";
 	string newChoice = "";
-
-	for (int i=0 ; i<choice.length() ; i++){
-		if (guess[i] == choice[i]){
-			bulls++;
-		}else {
-			newGuess += guess[i];
-			newChoice += choice[i];
+	if (choice.length() < guess.length()){
+		for (int i=0 ; i<choice.length() ; i++){
+			if (guess[i] == choice[i]){
+				bulls++;
+			}else {
+				newGuess += guess[i];
+				newChoice += choice[i];
+			}
+		}
+	}
+	else{
+		for (int i=0 ; i<guess.length() ; i++){
+			if (guess[i] == choice[i]){
+				bulls++;
+			}else {
+				newGuess += guess[i];
+				newChoice += choice[i];
+			}
 		}
 	}
 
-	for (int i=0 ; i<newChoice.length() ; i++){
+	for (int i=0 ; i<newGuess.length() ; i++){
 		for (int j=0 ; j<newChoice.length() ; j++){
 			if (newGuess[i] == newChoice[j]){
 				pgia++;
